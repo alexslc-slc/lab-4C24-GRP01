f43d60ef064578040754cc3e55562f430f7b0e73
alexslc-slc <esteban.santillan@tecsup.edu.pe>
2026-03-25 16:55:30 -0500

    StudentCtrl.java

--- StudentCtrl.java
+++ StudentCtrl.java
@@ -1,6 +1,5 @@
 public class StudentCtrl {
     public static void main(String[] args) {
         System.out.println("Student Controller");
-        System.out.println("Versión actualizada por Responsable");
     }
 }
